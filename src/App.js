7183d6918f592dccab1e299ef19e277e8f8ad8e0
Nasina-Srilahari <nasinasrilahariias@gmail.com>
2023-05-31 09:49:22 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import institutionLogo from './assets/institution-logo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
+
+
 const App = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
   const [username, setUsername] = useState('');
   const [selectedSchedule, setSelectedSchedule] = useState(null);
-  const [teacherInfo, setTeacherInfo] = useState({ teacherName: '', teacherId: '', schedule: [] });
+  const [teacherInfo, setTeacherInfo] = useState({ teacherName: '', teacherId: '' });
+
 
   // Dummy data for the teacher's schedule
   const teacherSchedule = [
-    { date: '2023-05-30', className: 'Math', time: '09:00 AM - 10:30 AM' },
-    { date: '2023-05-31', className: 'Science', time: '11:00 AM - 12:30 PM' },
-    { date: '2023-06-01', className: 'English', time: '02:00 PM - 03:30 PM' },
+    { date: selectedDate, className: 'Math', time: '09:00 AM - 10:30 AM' },
+    { date: selectedDate, className: 'Science', time: '11:00 AM - 12:30 PM' },
+    { date: selectedDate, className: 'English', time: '02:00 PM - 03:30 PM' },
   ];
+
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -26,98 +30,112 @@ const App = () => {
     setUsername(enteredUsername);
     setLoggedIn(true);
 
-    // Dummy teacher's information (replace with actual authentication logic)
     const loggedInTeacher = {
       teacherName: enteredUsername,
       teacherId: '123456',
       schedule: teacherSchedule,
     };
-
+  
     setTeacherInfo(loggedInTeacher);
     setSelectedSchedule(teacherSchedule);
   };
 
+
+  };
+
   const handleLogout = () => {
+    // Perform logout here
     setLoggedIn(false);
     setSelectedDate(null);
   };
 
   const handleDateSelect = (date) => {
-    const schedule = teacherInfo.schedule.filter((item) => item.date === date);
-
     setSelectedDate(date);
-    setSelectedSchedule(schedule.length > 0 ? schedule : null);
   };
 
   return (
     <div className="App">
       <header>
-        <nav className="navtop">
-          <div className="inside-mav">
-            <img src={institutionLogo} alt="InstitutionLogo" title="institution logo" />
-            <h1>Institution Name</h1>
-            {loggedIn ? <button onClick={handleLogout}>Logout</button> : null}
-          </div>
-        </nav>
+      <nav className="navtop">
+      <div className="inside-mav">
+        <img src={institutionLogo} alt="InstitutionLogo" title="institution logo"/>
+        <h1>Institution Name</h1>
+        {loggedIn ? (
+        <button onClick={handleLogout}>Logout</button>):null}
+      </div>
+      </nav>
       </header>
 
       {loggedIn ? (
         <div>
           <section>
             <h3>Welcome, {teacherInfo.teacherName}!</h3>
-            <p>ID: {teacherInfo.teacherId}</p>
+            {/* <p>ID: {teacherInfo.teacherId}</p> */}
+            <p>ID: 123456</p>
           </section>
 
           <section>
-            <div className="dateselection">
-              <h3>Select a Date</h3>
-              <DatePicker selected={selectedDate} onChange={(date) => handleDateSelect(date)} />
-            </div>
+          <div className="dateselection" >
+            <h3>Select a Date</h3>
+            <input type="date" onChange={(e) => handleDateSelect(e.target.value)} />
+          </div>
           </section>
 
           <section>
+          
             <h2>Schedule</h2>
-            {selectedSchedule ? (
-              <pre>{JSON.stringify(selectedSchedule, null, 2)}</pre>
+            {selectedDate ? (
+              <table className="center-table" >
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Class Name</th>
+                    <th>Time</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {teacherSchedule.map((schedule) => (
+                    <tr key={schedule.date}>
+                      <td>{schedule.date}</td>
+                      <td>{schedule.className}</td>
+                      <td>{schedule.time}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             ) : (
-              <p>No schedule available for the selected date.</p>
+              <p>Please select a date to view the schedule.</p>
             )}
           </section>
-
+          
+ 
           <footer>
-            <a href="https://www.google.com">
-              <FontAwesomeIcon icon={faInstagram} style={{ color: '#E4405F', fontSize: '24px' }} />
-            </a>
-            <a href="https://www.google.com">
-              <FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2', fontSize: '24px' }} />
-            </a>
-            <a href="https://www.google.com">
-              <FontAwesomeIcon icon={faFacebook} style={{ color: '#1877F2', fontSize: '24px' }} />
-            </a>
+            <a href="https://www.google.com"><FontAwesomeIcon icon={faInstagram} style={{ color: '#E4405F', fontSize: '24px'}}/></a>
+            <a href="https://www.google.com"><FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2', fontSize: '24px'}}/></a>
+            <a href="https://www.google.com"><FontAwesomeIcon icon={faFacebook} style={{ color: '#1877F2', fontSize: '24px'}}/></a>
           </footer>
+
         </div>
       ) : (
         <div>
+        
           <section className="center-section">
-            <h2>Login</h2>
+          <h2>Login</h2>
             <form onSubmit={handleLogin}>
-              <input type="text" placeholder="Username" name="username" />
+              <input type="text" placeholder="Username" name="username"/>
               <input type="password" placeholder="Password" />
-              <button type="submit">Login</button>
+              <button  type="submit">Login</button>
             </form>
           </section>
 
+
           <footer>
-            <a href="https://www.google.com">
-              <FontAwesomeIcon icon={faInstagram} style={{ color: '#E4405F', fontSize: '24px' }} />
-            </a>
-            <a href="https://www.google.com">
-              <FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2', fontSize: '24px' }} />
-            </a>
-            <a href="https://www.google.com">
-              <FontAwesomeIcon icon={faFacebook} style={{ color: '#1877F2', fontSize: '24px' }} />
-            </a>
+            <a href="https://www.google.com"><FontAwesomeIcon icon={faInstagram} style={{ color: '#E4405F', fontSize: '24px'}}/></a>
+            <a href="https://www.google.com"><FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2', fontSize: '24px'}}/></a>
+            <a href="https://www.google.com"><FontAwesomeIcon icon={faFacebook} style={{ color: '#1877F2', fontSize: '24px'}}/></a>
           </footer>
+
+
         </div>
       )}
     </div>
